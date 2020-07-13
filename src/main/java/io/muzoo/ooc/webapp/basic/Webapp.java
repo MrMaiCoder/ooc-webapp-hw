335d1339b040d99ce47f29541a9427665a2d3f6b
Phairat Lin <yangsheng104@gmail.com>
2020-07-13 23:31:12 +0700

    Changed port to the correct one

--- src/main/java/io/muzoo/ooc/webapp/basic/Webapp.java
+++ src/main/java/io/muzoo/ooc/webapp/basic/Webapp.java
@@ -1,7 +1,5 @@
 package io.muzoo.ooc.webapp.basic;
 
-import io.muzoo.ooc.webapp.basic.security.SecurityService;
-import io.muzoo.ooc.webapp.basic.servlets.AbstractRoutableHttpServlet;
 import io.muzoo.ooc.webapp.basic.servlets.ServletRouter;
 import org.apache.catalina.Context;
 import org.apache.catalina.LifecycleException;
@@ -15,7 +13,7 @@ public class Webapp {
 
     public static void main(String[] args) {
         Tomcat tomcat = new Tomcat();
-        tomcat.setPort(8082);
+        tomcat.setPort(80);
 
         File doceBase = new File("src/main/webapp/");
         doceBase.mkdirs();
